a0683976de4b226fec71ca939d0d01ffb0d84678
LeoFuna <leo.funa@gmail.com>
2021-11-09 18:51:46 -0300

    feat: adicionando try catch no getAll para tratamento de erro

--- src/services/fetchApi.js
+++ src/services/fetchApi.js
@@ -1,7 +1,11 @@
-const BASE_URL = 'https://my-json-server.typicode.com/tractian/fake-api';
+const BASE_URL = 'https://my-json-server.typicode.com/tractian/fake-ap';
 
 export const getAll = async (endpoint) => {
-  const dataFromApi = await fetch(`${BASE_URL}/${endpoint}`);
-  const allData = await dataFromApi.json();
-  return allData;
+  try {
+    const dataFromApi = await fetch(`${BASE_URL}/${endpoint}`);
+    const allData = await dataFromApi.json();
+    return allData;
+  } catch (err) {
+    console.log('TO AQUI'); // porque não entra aqui?
+  }
 };
