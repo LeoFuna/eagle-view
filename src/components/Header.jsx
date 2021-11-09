ce41269369fb45517f2c0c801f5cc3b8c09288b3
LeoFuna <leo.funa@gmail.com>
2021-11-09 18:24:28 -0300

    feat: inicial estruturaçao das divs para o header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,25 @@ import React from 'react';
 
 function Header() {
   return (
-    <div>
-      Sou o Header
-    </div>
+    <header>
+      <div>
+        <h1>Eagle View</h1>
+      </div>
+      <div>
+        <h1>Empresa</h1>
+        <select name="" id="">
+          <option value="">opcao 1</option>
+          <option value="">opcao 2</option>
+        </select>
+      </div>
+      <div>
+        <h1>Unidade</h1>
+        <select name="" id="">
+          <option value="">opcao 1</option>
+          <option value="">opcao 2</option>
+        </select>
+      </div>
+    </header>
   );
 }
 
