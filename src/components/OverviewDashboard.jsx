56c1b713bf9ca3f5ab321d6a5db88fb95fbb0540
LeoFuna <leo.funa@gmail.com>
2021-11-11 10:44:59 -0300

    feat: adicionando compnente de dashboard com primeiros testes no mesmo

---
+++ src/components/OverviewDashboard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function OverviewDashbord() {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <div>
+        <p>Sou o primeiro gráfico</p>
+      </div>
+      <div>
+        <p>Sou o segundo gráfico</p>
+      </div>
+      <div>
+        <p>Sou o terceiro gráfico</p>
+      </div>
+      <div>
+        <p>Sou o detalhes do equipamento</p>
+      </div>
+    </div>
+  );
+}
+
+export default OverviewDashbord;
