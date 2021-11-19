7ffaf22963a0b209f4249eaaff3ef88c021af000
LeoFuna <leo.funa@gmail.com>
2021-11-19 10:35:01 -0300

    feat: imagem renderizada sem estilo

--- src/components/AllAssetDetails.jsx
+++ src/components/AllAssetDetails.jsx
@@ -4,7 +4,9 @@ function AllAssetDetails({ assetData }) {
   console.log(assetData)
   return(
     <div>
-      <div>div da imagem</div>
+      <div>
+        <img  style={{ width: '75%', borderRadius: '30px' }} src={ assetData.image } alt={ `Imagem ${ assetData.name }`} />
+      </div>
       <div>div dos status</div>
       <div>div dos detalhes</div>
     </div>
