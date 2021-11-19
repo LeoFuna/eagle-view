156cb1993d273b0277c1d544c892a754c2182486
LeoFuna <leo.funa@gmail.com>
2021-11-19 10:39:25 -0300

    feat: divisao das informacoes importantes e renderizaçao das mesmas, sem estilo

--- src/components/AllAssetDetails.jsx
+++ src/components/AllAssetDetails.jsx
@@ -7,7 +7,20 @@ function AllAssetDetails({ assetData }) {
       <div>
         <img  style={{ width: '75%', borderRadius: '30px' }} src={ assetData.image } alt={ `Imagem ${ assetData.name }`} />
       </div>
-      <div>div dos status</div>
+      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+        <div>
+          <h3>Modelo</h3>
+          <p>{ assetData.model }</p>
+        </div>
+        <div>
+          <h3>Saúde</h3>
+          <p>{ assetData.healthscore }</p>
+        </div>
+        <div>
+          <h3>Status</h3>
+          <p>{ assetData.status }</p>
+        </div>
+      </div>
       <div>div dos detalhes</div>
     </div>
   );
