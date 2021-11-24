7fac0623e564b62936b0927a5f04900f088858e1
LeoFuna <leo.funa@gmail.com>
2021-11-24 10:12:56 -0300

    test: alterando a renderizaçao no teste do Header para o App ao inves do Header diretamente

--- src/tests/OverviewDashbord.test.js
+++ src/tests/OverviewDashbord.test.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from '../App';
+import OverviewDashboard from '../components/OverviewDashboard';
 
 describe('Ao renderizar ao aplicação', () => {
-  test('todos os elementos do Header devem estar à mostra', async () => {
-    const { getByText } = render(<App />);
+  test('todos os elementos do Header devem estar à mostra', () => {
+    const { getByText, getAllByDisplayValue } = render(<OverviewDashboard />);
     expect(getByText('Eagle View')).toBeInTheDocument();
     expect(getByText('Empresa')).toBeInTheDocument();
     expect(getByText('Unidade')).toBeInTheDocument();
+    expect(getAllByDisplayValue('Todas').length).toBe(2);
   });
 })
